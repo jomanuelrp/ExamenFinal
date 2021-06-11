197d5560d58a5eeaf73713e961862adb2be4f2c6
jomanuelrp <jomanuelrp@gmail.com>
2021-06-11 11:29:23 -0600

    Update examen_final.py

--- examen_final.py
+++ examen_final.py
@@ -3,10 +3,14 @@ import numpy as np
 import matplotlib.pylab as plt
 from pandas.io.pytables import dropna_doc
 
+#importar y visualizar datos
+
+#Datos extraidos de https://sigsa.mspas.gob.gt/datos-de-salud/morbilidad/principales-causas-de-morbilidad
 
 
 df = pd.read_excel("morbilidad_2019.xlsx", header=4, usecols=(2,3), nrows=21)
 
+#imprime solo los datos a utilizar (columna 2 y 3)
 print(df)
 
 # Grafica de datos
